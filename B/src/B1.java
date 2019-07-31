101f4d595ab665ea5ba74c20215d4af3e5f8087c
Jeka <kotyurginz@mail.ru>
2019-07-31 21:09:56 +0300

    second

---
+++ B/src/B1.java
@@ -0,0 +1,24 @@
+
+public class B1 {
+
+	public static void main(String[] args) {
+		
+System.out.print(fib(30));
+	}
+
+	
+	
+	private static int fib (int i)  {
+		while (fib(i)<4000000) {
+		if(i==0) {
+			return 0;
+		  } 
+		if(i==1) {
+			return 1;
+			
+		}
+		else return fib(i-2)+fib(i-1);
+	   }
+		return i;
+	}
+}
