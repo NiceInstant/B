3fdd33c47db715f4e4c003383e4fe93613b0fce0
Jeka <kotyurginz@mail.ru>
2019-08-04 22:26:07 +0300

    B

--- B/src/B1.java
+++ B/src/B1.java
@@ -3,7 +3,7 @@ public class B1 {
 
 	public static void main(String[] args) {
 		
-System.out.print(fib(20));
+System.out.print(fib(30));
 	}
 
 	private static int fib (int i)  {
@@ -12,7 +12,7 @@ System.out.print(fib(20));
 			return 0;
 		  } 
 		if(i==1) {
-			return 1;
+			return 1;			
 		}
 		else return fib(i-2)+fib(i-1);
 	   }
